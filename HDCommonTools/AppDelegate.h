5a854a9a8a130e06a087015954afa41234375820
Damon <dong765@qq.com>
2018-02-15 18:52:14 +0800

    增加demo
    增加demo

---
+++ HDCommonTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDCommonTools
+//
+//  Created by Damon on 2018/2/14.
+//  Copyright © 2018年 damon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
