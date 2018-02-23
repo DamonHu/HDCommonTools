bbed664b1cce41e4f87690872ab4e63a0c7b27df
Damon <dong765@qq.com>
2018-02-23 15:38:37 +0800

    增加常用宏定义
    增加常用宏定义

---
+++ HDCommonTools/HDCommonTools/HDCommonDefine.h
@@ -0,0 +1,67 @@
+//
+//  HDCommonDefine.h
+//  HDCommonTools
+//
+//  Created by Damon on 2018/2/23.
+//  Copyright © 2018年 damon. All rights reserved.
+//
+
+/**
+ *常用的宏定义
+ *快捷实现常用功能
+*/
+#ifndef HDCommonDefine_h
+#define HDCommonDefine_h
+
+#pragma mark -
+#pragma mark - 对象引用
+///弱引用
+#define HDWEAKSELF __weak typeof(self) weakSelf = self
+///强引用
+#define HDSTRONGSELF __strong typeof(weakSelf) strongSelf = weakSelf
+
+#pragma mark -
+#pragma mark - log输出
+//log输出，当为true时输出log，false不输出log
+#define HDDEBUG_MODE true
+
+#if HDDEBUG_MODE
+#define HDDebugLog( s, ... ) NSLog( @"\n↓↓↓↓↓↓↓↓\n<%p %@:(%d)> \n%s\n%@\n↑↑↑↑↑↑↑↑", __FILE__, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, __FUNCTION__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+#else
+#define HDDebugLog( s, ... )
+#endif
+
+#pragma mark -
+#pragma mark - 界面
+/*
+ *  UIColor
+ */
+///16进制颜色转为UIColor
+#define HDColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+///16进制颜色转为UIColor，设置透明度
+#define HDColorFromRGBA(rgbValue, _A) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:_A]
+///通过数值转为UIColor
+#define HDColorWithRGB(_R,_G,_B)        ((UIColor *)[UIColor colorWithRed:_R/255.0 green:_G/255.0 blue:_B/255.0 alpha:1.0])
+///通过数值转为UIColor，设置透明度
+#define HDColorWithRGBA(_R,_G,_B,_A)    ((UIColor *)[UIColor colorWithRed:_R/255.0 green:_G/255.0 blue:_B/255.0 alpha:_A])
+/*
+ *  Screen size
+ */
+///屏幕宽度
+#define HDScreenWidth   [UIScreen mainScreen].bounds.size.width
+///屏幕高度
+#define HDScreenHeight  [UIScreen mainScreen].bounds.size.height
+///状态栏当前高度
+#define HD_Portrait_Status_Height [UIApplication sharedApplication].statusBarFrame.size.height //状态栏高度
+
+// 判断是否是iPhone X
+#define iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+// 状态栏默认高度
+#define HD_Default_Portrait_Status_Height (iPhoneX ? 44.f : 20.f)
+// 导航栏默认高度
+#define HD_Default_Portrait_NAVIGATION_BAR_HEIGHT (iPhoneX ? 88.f : 64.f)
+// tabBar默认高度
+#define HD_Default_Portrait_TAB_BAR_HEIGHT (iPhoneX ? (49.f+34.f) : 49.f)
+// home indicator
+#define HD_Default_Portrait_HOME_INDICATOR_HEIGHT (iPhoneX ? 34.f : 0.f)
+#endif /* HDCommonDefine_h */
