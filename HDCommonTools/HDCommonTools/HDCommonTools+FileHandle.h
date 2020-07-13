9876645bb9a4654c4385c43c5c7c82b79fb5393a
Damon <dong765@qq.com>
2020-07-13 14:07:47 +0800

    更新文件夹路径读取
    更新文件夹路径读取

--- HDCommonTools/HDCommonTools/HDCommonTools+FileHandle.h
+++ HDCommonTools/HDCommonTools/HDCommonTools+FileHandle.h
@@ -8,18 +8,22 @@
 
 #import "HDBaseCommonTools.h"
 
+typedef NS_ENUM(NSUInteger, HDFileDirectoryType) {
+    kHDFileDirectoryTypeHome,       //程序主目录
+    kHDFileDirectoryTypeDocuments,  //应用中用户数据可以放在这里，iTunes备份和恢复的时候会包括此目录
+    kHDFileDirectoryTypeTmp,        //存放临时文件，iTunes不会备份和恢复此目录，此目录下文件可能会在应用退出后删除
+    kHDFileDirectoryTypeCaches,     //存放缓存文件，iTunes不会备份此目录，此目录下文件不会在应用退出删除
+};
+
 @interface HDCommonTools (FileHandle)
 
 #pragma mark -
 #pragma mark - 文件处理管理类
+///获取文件夹路径
+- (NSURL *)getFileDirectoryWithType:(HDFileDirectoryType)directoryType;
 
-///将Data内容写入本地保存，重新命名，返回保存过的路径
-// Write the Data content to local save, rename, and return the saved path
-- (NSString *)saveData:(NSData *)data withFileName:(NSString *)fileName;
-
-///在Document创建子文件夹并返回创建后的路径
-//Create a subfolder in Document And return to the created path
-- (NSString *)createDocumentSubFolderWithName:(NSString *)folderName;
+///在指定文件夹中创建文件夹
+- (NSURL *)createFileDirectoryWithType:(HDFileDirectoryType)directoryType andDirectoryName:(NSString *)directoryName;
 
 ///获取在Document文件夹里的或者子文件夹里面对应文件名的路径
 //Get the path to the file name in the Document folder or in the subfolder
@@ -36,4 +40,12 @@
 ///指定路径的文件夹总大小，单位b
 ///get the folder size at path,the Unit is b
 - (float)folderSizeAtPath:(NSString *)folderPath;
+
+///将Data内容写入本地保存，重新命名，返回保存过的路径
+// Write the Data content to local save, rename, and return the saved path
+- (NSString *)saveData:(NSData *)data withFileName:(NSString *)fileName DEPRECATED_MSG_ATTRIBUTE("该接口即将被废弃，使用[data writeToFile:filePath atomically:YES];代替");
+
+///在Document创建子文件夹并返回创建后的路径
+//Create a subfolder in Document And return to the created path
+- (NSString *)createDocumentSubFolderWithName:(NSString *)folderName DEPRECATED_MSG_ATTRIBUTE("使用createFileDirectoryWithType:andDirectoryName");
 @end
--- HDCommonTools/HDCommonTools/HDCommonTools+FileHandle.m
+++ HDCommonTools/HDCommonTools/HDCommonTools+FileHandle.m
@@ -11,37 +11,40 @@
 @implementation HDCommonTools (FileHandle)
 #pragma mark -
 #pragma mark - 文件处理管理类
-
-///将Data内容写入本地保存，重新命名，返回保存过的路径
-// Write the Data content to local save, rename, and return the saved path
-- (NSString *)saveData:(NSData *)data withFileName:(NSString* )fileName {
-    NSArray *paths=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
-    NSString *documentsDirectory=[paths objectAtIndex:0];
-    NSString *savedImagePath=[documentsDirectory stringByAppendingPathComponent:fileName];
-    [data writeToFile:savedImagePath atomically:YES];
-    return savedImagePath;
+///获取文件夹路径
+- (NSURL *)getFileDirectoryWithType:(HDFileDirectoryType)directoryType {
+    NSString *homePath = NSHomeDirectory();
+    switch (directoryType) {
+    case kHDFileDirectoryTypeHome:
+        return [NSURL fileURLWithPath:homePath];
+    case kHDFileDirectoryTypeDocuments:
+        return [NSURL fileURLWithPath:[homePath stringByAppendingPathComponent:@"Documents"]];
+    case kHDFileDirectoryTypeTmp:
+        return [NSURL fileURLWithPath:[homePath stringByAppendingPathComponent:@"tmp"]];
+    case kHDFileDirectoryTypeCaches:
+        return [NSURL fileURLWithPath:[homePath stringByAppendingPathComponent:@"Library/Caches"]];
+    }
 }
 
-///在Document创建子文件夹并返回创建后的路径
-//Create a subfolder in Document And return to the created path
-- (NSString *)createDocumentSubFolderWithName:(NSString *)folderName {
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *path = [paths objectAtIndex:0];
-    NSString *folderPath = [path stringByAppendingPathComponent:folderName];
+///在指定文件夹中创建文件夹
+- (NSURL *)createFileDirectoryWithType:(HDFileDirectoryType)directoryType andDirectoryName:(NSString *)directoryName {
+    NSURL *path = [self getFileDirectoryWithType:directoryType];
+    NSURL *folderPath = [path URLByAppendingPathComponent:directoryName isDirectory:true];
     
     NSFileManager *fileManager = [NSFileManager defaultManager];
     BOOL isDir = FALSE;
-    BOOL isDirExist = [fileManager fileExistsAtPath:folderPath isDirectory:&isDir];
+    BOOL isDirExist = [fileManager fileExistsAtPath:folderPath.path isDirectory:&isDir];
     
-    if(!(isDirExist && isDir))
-        {
-        BOOL bCreateDir = [fileManager createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:nil];
+    if(!(isDirExist && isDir)) {
+        NSError *error;
+        BOOL bCreateDir = [fileManager createDirectoryAtURL:folderPath withIntermediateDirectories:YES attributes:nil error: &error];
+        if (error) {
+            NSLog(@"%@", error.localizedDescription);
+        }
         if(!bCreateDir){
             NSLog(@"Failure to create a folder");
-            return @"";
         }
-        return folderPath;
-        }
+    }
     return folderPath;
 }
 
@@ -98,5 +101,38 @@
         folderSize += [self fileSizeAtPath:fileAbsolutePath];
     }
     return folderSize;
+}
+
+///将Data内容写入本地保存，重新命名，返回保存过的路径
+// Write the Data content to local save, rename, and return the saved path
+- (NSString *)saveData:(NSData *)data withFileName:(NSString* )fileName DEPRECATED_MSG_ATTRIBUTE("该接口即将被废弃，使用[data writeToFile:filePath atomically:YES];代替") {
+    NSArray *paths=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,NSUserDomainMask,YES);
+    NSString *documentsDirectory=[paths objectAtIndex:0];
+    NSString *savedImagePath=[documentsDirectory stringByAppendingPathComponent:fileName];
+    [data writeToFile:savedImagePath atomically:YES];
+    return savedImagePath;
+}
+
+///在Document创建子文件夹并返回创建后的路径
+//Create a subfolder in Document And return to the created path
+- (NSString *)createDocumentSubFolderWithName:(NSString *)folderName {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *path = [paths objectAtIndex:0];
+    NSString *folderPath = [path stringByAppendingPathComponent:folderName];
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    BOOL isDir = FALSE;
+    BOOL isDirExist = [fileManager fileExistsAtPath:folderPath isDirectory:&isDir];
+    
+    if(!(isDirExist && isDir))
+        {
+        BOOL bCreateDir = [fileManager createDirectoryAtPath:folderPath withIntermediateDirectories:YES attributes:nil error:nil];
+        if(!bCreateDir){
+            NSLog(@"Failure to create a folder");
+            return @"";
+        }
+        return folderPath;
+        }
+    return folderPath;
 }
 @end
